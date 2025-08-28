bc09cacfa60e03aa66a21d4d673ba66bec451398
Isabella Caroline <isabellab.limaa2@gmail.com>
2025-08-28 11:35:28 -0300

    Add files via upload

---
+++ trabalho/isabella manha/isabella/cadastro.js
@@ -0,0 +1,28 @@
+    function cadastrar() {
+        const names = ["Isabella", "Enzo" , "Theo"];
+        const sem = ["1234", "4321" , "8910"]
+
+        const mensagem = document.getElementById("mensagem");
+        const nome = document.getElementById("nome").value;
+        const acesso = document.getElementById("senha").value;
+        
+        let correto = false; 
+
+        for(let i = 0; i < names.length; i++) {
+            if (nome === names[i] && acesso === sem[i]) {
+                correto = true;
+                mensagem.innerHTML = "redirecionando...";
+                setTimeout(() => {
+                window.location.href = "cadastro2.html"
+            }, 1000);
+               
+                
+            }
+        }
+    
+    if (!correto){
+        mensagem.innerHTML = "Nome ou senha incorretos!";
+    } 
+    
+}
+    
